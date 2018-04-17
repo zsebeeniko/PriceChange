fb95a742938fcd9a9c87890d9c52431c0f7a418b
Eniko Zsebe <eniko.zsebe@accenture.com>
2018-04-17 17:11:35 +0300

    Implementation of the tutorial https://www.youtube.com/watch?v=3aRlYjSQPc8

---
+++ SmartPrice/SmartPriceAndroid/MainActivity.cs
@@ -0,0 +1,116 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Gms.Vision;
+using Android.Gms.Vision.Texts;
+using Android.Util;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android;
+using Android.Content.PM;
+using static Android.Gms.Vision.Detector;
+using System.Text;
+
+namespace SmartPriceAndroid
+{
+    [Activity(Label = "SmartPriceAndroid", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity, ISurfaceHolderCallback, IProcessor
+    {
+        private SurfaceView cameraView;
+        private TextView textView;
+        private CameraSource cameraSource;
+        private const int RequestCameraPermissionID = 1001;
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            switch(requestCode)
+            {
+                case RequestCameraPermissionID:
+                    {
+                        if(grantResults[0] == Permission.Granted)
+                        {
+                            cameraSource.Start(cameraView.Holder);
+                        }
+                    }
+                    break;
+            }
+
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            cameraView = FindViewById<SurfaceView>(Resource.Id.surface_view);
+            textView = FindViewById<TextView>(Resource.Id.text_view);
+
+            TextRecognizer textRecognizer = new TextRecognizer.Builder(ApplicationContext).Build();
+            if (!textRecognizer.IsOperational)
+                Log.Error("MainActivity Activity", "Detector dependencies are not yet available");
+            else
+            {
+                cameraSource = new CameraSource.Builder(ApplicationContext, textRecognizer)
+                    .SetFacing(CameraFacing.Back)
+                    .SetRequestedPreviewSize(1280, 1024)
+                    .SetRequestedFps(2.0f)
+                    .SetAutoFocusEnabled(true)
+                    .Build();
+
+                cameraView.Holder.AddCallback(this);
+                textRecognizer.SetProcessor(this);
+            }
+
+        }
+
+        public void SurfaceChanged(ISurfaceHolder holder, [GeneratedEnum] Format format, int width, int height)
+        {
+            
+        }
+
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            if(ActivityCompat.CheckSelfPermission(ApplicationContext, Manifest.Permission.Camera) != Android.Content.PM.Permission.Granted)
+            {
+                //Request permission
+                ActivityCompat.RequestPermissions(this, new string[]
+                {
+                    Android.Manifest.Permission.Camera
+                }, RequestCameraPermissionID);
+                return;
+            }
+            cameraSource.Start(cameraView.Holder);
+        }
+
+        public void SurfaceDestroyed(ISurfaceHolder holder)
+        {
+            cameraSource.Stop();
+        }
+
+        public void ReceiveDetections(Detections detections)
+        {
+            SparseArray items = detections.DetectedItems;
+            if(items.Size() != 0)
+            {
+                textView.Post(() => {
+                    StringBuilder strBuilder = new StringBuilder();
+                    for(int i=0; i<items.Size(); ++i)
+                    {
+                        strBuilder.Append((TextBlock) items.ValueAt(i));
+                        strBuilder.Append("\n");
+                    }
+                    textView.Text = strBuilder.ToString();
+               });
+            }
+        }
+
+        public void Release()
+        {
+            
+        }
+    }
+}
+
